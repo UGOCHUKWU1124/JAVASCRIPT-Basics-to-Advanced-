4000e000affd91cf43187c9021fac66a980df785
ugochukwu <ugochukwutemitayo@gmail.com>
2025-03-03 20:42:13 +0100

    Added continue

---
+++ Continue/continue.js
@@ -0,0 +1,9 @@
+for (var i=1;i<=10;i++)
+    {
+       if(i==6)
+          {
+              continue; //stops the loop at 6 then continues the loop
+          }
+        console.log(i);
+    }
+    
