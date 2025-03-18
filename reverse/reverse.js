17ddd97006c64c315d452a5d5358de303670a510
Ugochukwu <ugochukwutemitayo@gmail.com>
2025-03-18 19:21:34 +0100

    Added reverse array

---
+++ reverse/reverse.js
@@ -0,0 +1,9 @@
+//it reverses the entire array and overwrites the existing array with reversed array
+const products = [
+    {productName:"TV", price:87000},
+    {productName:"Monitor", price:6000},
+    {productName:"ipad", price:3400},
+    {productName:"phone", price:2000}
+]; 
+console.log("before reverse", products)
+console.log("after reverse",products.reverse());
